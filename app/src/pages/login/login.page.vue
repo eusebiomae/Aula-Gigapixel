e20bf61725ff76364993ac7ff32fcc65cb405c54
mayitoae84 <mario.enriquez@gigapixel.com.br>
2021-02-24 12:07:54 -0300

    no message

--- app/src/pages/login/login.page.vue
+++ app/src/pages/login/login.page.vue
@@ -1,17 +1,12 @@
 <template>
-	<div class="q-pa-md" style="max-width: 400px">
-
-    <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md"
-    >
-		<q-input
+  <div class="q-pa-md" style="max-width: 400px">
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input
         filled
         v-model="login.email"
         label="E-mail *"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Digite seu email']"
+        :rules="[(val) => (val && val.length > 0) || 'Digite seu email']"
       />
 
       <q-input
@@ -20,53 +15,61 @@
         v-model="login.password"
         label="Senha *"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Digite sua Senha']"
+        :rules="[(val) => (val && val.length > 0) || 'Digite sua Senha']"
       />
-			<div>
-        <q-btn @onclick="onSubmit()" label="Submit" type="submit" color="primary"/>
-        <q-btn @onclick="onReset()" label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      <div>
+        <q-btn
+          @onclick="onSubmit()"
+          label="Submit"
+          type="submit"
+          color="primary"
+        />
+        <q-btn
+          @onclick="onReset()"
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
       </div>
-		</q-form>
-	</div>
+    </q-form>
+  </div>
 </template>
 
 <script>
 export default {
-	data () {
+  data() {
     return {
       login: {
         email: null,
         password: null,
-      }
-    }
+      },
+    };
   },
 
   methods: {
-    onSubmit () {
-
+    onSubmit() {
       return this.$axios({
-        method: 'post',
-        url: 'http://127.0.0.1:8000/api/auth',
-        data: this.login
-      }).then (resp => {
+        method: "post",
+        url: "http://127.0.0.1:8000/api/auth",
+        data: this.login,
+      }).then((resp) => {
+        this.$q.sessionStorage.set("gp_token", resp.data);
 
-        this.$q.sessionStorage.set('gp_token', resp.data)
+        this.$router.push({ name: "index" });
 
-        this.$router.push({name: 'index'})
-
-        return resp
-      }
-      )
+        return resp;
+      });
     },
 
-    onReset () {
-      this.email = null
-      this.password = null
-    }
-  }
-}
+    onReset() {
+      this.email = null;
+      this.password = null;
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
